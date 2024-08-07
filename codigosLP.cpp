501285d0accc33edb102bcd05fd5bf428215fbca
reversecod <otavioresendeoficial@gmail.com>
2024-08-07 10:22:00 -0300

    Código bubblesort

--- codigosLP.cpp
+++ codigosLP.cpp
@@ -1,48 +1,44 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#define TAM 10
 
 using namespace std;
 
-double ler_coeficiente(string nome) {
-    double coef;
-    cout << "Digite o coeficiente " << nome << ": ";
-    cin >> coef;
-    return coef;
+void imprimir_vetor(int vetor [TAM]){
+    for (int i = 0; i < TAM; i++){
+        cout << "|" << vetor[i] << "|";
+    }
 }
 
-double calculo_delta(double a, double b, double c) {
-    return (b * b) - (4 * a * c);
-}
-
-void calcular_raizes(double a, double b, double delta, double &x1, double &x2) {
-    if (delta < 0) {
-    cout << "Não é possível calcular raíz negativa.\n";
-
-} else {
-    x1 = (-b + sqrt(delta)) / (2 * a);
-    x2 = (-b - sqrt(delta)) / (2 * a);
-}
-
-}
-void exibir(double delta, double x1, double x2) {
-    cout << "\nDelta: " << delta << endl;
-    cout << "Raiz 1: " << x1 << endl;
-    cout << "Raiz 2: " << x2 << endl;
-
+void bubblesort(int vetor[TAM]){
+    int aux;
+    for (int i = 0; i < TAM; i++){
+        for (int j = i+1; j < TAM; j++){
+            if (vetor[i]>vetor[j]){
+                aux = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = aux;
+            }
+        }
+    }
 }
 
 int main() {
-double a, b, c, delta, x1, x2;
+    int vetor[TAM];
 
-    a = ler_coeficiente("a");
-    b = ler_coeficiente("b");
-    c = ler_coeficiente("c");
-    delta = calculo_delta(a, b, c);
-    calcular_raizes(a, b, delta, x1, x2);
+    for (int i = 0; i < TAM; i++){
+        cout << "Digite o " << i + 1 << "° numero aleatorio: ";
+        cin >> vetor[i];
+    }
 
-    exibir(delta, x1, x2);
+    cout << "\nVetor original: ";
+    imprimir_vetor(vetor);
 
-return 0;
+    bubblesort(vetor);
 
-} 
+    cout << "\n\nVetor ordenado: ";
+    imprimir_vetor(vetor);
+
+    return 0;
+}
