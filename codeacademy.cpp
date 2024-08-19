c0251531fd847be90ff25d2479c2b2f55a5331b5
reversecod <otavioresendeoficial@gmail.com>
2024-08-19 10:14:29 -0300

    Remoção dos 'std'

--- codeacademy.cpp
+++ codeacademy.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
+using namespace std;
 
-bool is_palindrome(const std::string& word){
+bool is_palindrome(const string& word){
 
-std::string reversed_word = "";
+string reversed_word = "";
 
 for(int i = word.size() - 1; i >= 0; i--){
   reversed_word += word[i];
 }
   if (reversed_word == word)
   {
-    std::cout << "This word is a palindrome.\n";
+    cout << "This word is a palindrome.\n";
     return true;
   }
-    std::cout << "This word is not a palindrome.\n";
+    cout << "This word is not a palindrome.\n";
     return false;   
 }
 
 int main(){
-  std::string word;
-  std::cout << "Enter the word that you want to see if it is a palindrome or not: ";
-  std::cin >> word;
+  string word;
+  cout << "Enter the word that you want to see if it is a palindrome or not: ";
+  cin >> word;
   is_palindrome(word);
 
   return 0;
