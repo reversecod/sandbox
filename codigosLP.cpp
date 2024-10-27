d5196c5b381b4346f6d666a1b876a702bcc0302f
reversecod <otavioresendeoficial@gmail.com>
2024-10-26 22:08:48 -0300

    Alteração do diretório sandbox

--- codigosLP.cpp
+++ codigosLP.cpp
@@ -1,44 +1,90 @@
 #include <iostream>
-#include <cstdlib>
-#include <cmath>
-#define TAM 10
+#include <string>
 
-using namespace std;
+struct PESSOA {
+    std::string nome;
+    int idade;
+    char sexo;
+    double salario;
+    double salarioNovo;
+};
 
-void imprimir_vetor(int vetor [TAM]){
-    for (int i = 0; i < TAM; i++){
-        cout << "|" << vetor[i] << "|";
-    }
-}
-
-void bubblesort(int vetor[TAM]){
-    int aux;
-    for (int i = 0; i < TAM; i++){
-        for (int j = i+1; j < TAM; j++){
-            if (vetor[i]>vetor[j]){
-                aux = vetor[i];
-                vetor[i] = vetor[j];
-                vetor[j] = aux;
-            }
-        }
-    }
-}
+void NovaLinhaStruct(PESSOA *p);
+void listarLinhasStruct(PESSOA *pessoas, int count);
+void ler_idade(PESSOA *p);
+void ler_sexo(PESSOA *p);
+void ler_nome(PESSOA *p);
+void ler_salario(PESSOA *p);
+double getAumento(double salario);
 
 int main() {
-    int vetor[TAM];
+    PESSOA pessoas[2];
+    PESSOA *pPessoa = pessoas;
 
-    for (int i = 0; i < TAM; i++){
-        cout << "Digite o " << i + 1 << "* numero aleatorio: ";
-        cin >> vetor[i];
-    }
+    int opcao;
+    do {
+        std::cout << "Menu:\n1 - Ler linha\n2 - Mostrar\n3 - Sair\nEscolha uma opção: ";
+        std::cin >> opcao;
 
-    cout << "\nVetor original: ";
-    imprimir_vetor(vetor);
-
-    bubblesort(vetor);
-
-    cout << "\n\nVetor ordenado: ";
-    imprimir_vetor(vetor);
+        switch (opcao) {
+            case 1:
+                for (int i = 0; i < 2; ++i) {
+                    NovaLinhaStruct(&pPessoa[i]);
+                }
+                break;
+            case 2:
+                listarLinhasStruct(pPessoa, 2);
+                break;
+            case 3:
+                std::cout << "Saindo...\n";
+                break;
+            default:
+                std::cout << "Opção inválida!\n";
+        }
+    } while (opcao != 3);
 
     return 0;
+}
+
+void NovaLinhaStruct(PESSOA *p) {
+    ler_nome(p);
+    ler_idade(p);
+    ler_sexo(p);
+    ler_salario(p);
+    p->salarioNovo = getAumento(p->salario);
+}
+
+void listarLinhasStruct(PESSOA *pessoas, int count) {
+    for (int i = 0; i < count; ++i) {
+        std::cout << "Nome: " << pessoas[i].nome 
+                  << ", Idade: " << pessoas[i].idade 
+                  << ", Sexo: " << pessoas[i].sexo 
+                  << ", Salário: " << pessoas[i].salario 
+                  << ", Novo Salário: " << pessoas[i].salarioNovo << std::endl;
+    }
+}
+
+void ler_idade(PESSOA *p) {
+    std::cout << "Digite a idade: ";
+    std::cin >> p->idade;
+}
+
+void ler_sexo(PESSOA *p) {
+    std::cout << "Digite o sexo (M/F): ";
+    std::cin >> p->sexo;
+}
+
+void ler_nome(PESSOA *p) {
+    std::cout << "Digite o nome: ";
+    std::cin.ignore();
+    std::getline(std::cin, p->nome);
+}
+
+void ler_salario(PESSOA *p) {
+    std::cout << "Digite o salário: ";
+    std::cin >> p->salario;
+}
+
+double getAumento(double salario) {
+    return salario * 1.1;
 }
